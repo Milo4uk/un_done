2af8d76317dac63ce1f109c8f1ac0413d47b7dd2
Mila <miloslava493@gmail.com>
2021-10-13 01:32:14 +0300

    Initial Commit

---
+++ Me/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Me
+//
+//  Created by Mila ✨ on 13.10.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
