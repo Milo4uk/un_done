fcab82d67aeaa7bd499d15c93eebdbdf37225c92
Milo4uk <92388527+Milo4uk@users.noreply.github.com>
2021-10-22 01:31:12 +0300

    Add files via upload

---
+++ ThatOne_3-1.c
@@ -0,0 +1,23 @@
+//
+//  ThatOne_3.c
+//  Me
+//
+//  Created by Mila ✨ on 15.10.2021.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    
+    int i, n;
+    
+    printf ("Insert number for loop\n");
+    scanf ("%d", &n);
+    
+    for (i = 0; i <= n; i++){
+        printf ("Value of the number: %d\n", i);
+    }
+    
+    return 0;
+}
