b394aed094d99ff70f52a60c73fe16a6417b330e
Milo4uk <92388527+Milo4uk@users.noreply.github.com>
2021-10-22 13:53:26 +0300

    Update Calculator_2-6.c

--- Calculator_2-6.c
+++ Calculator_2-6.c
@@ -9,42 +9,46 @@
 
 int main()
 {
-    
+
     double firstNum, secondNum, endRes;
     char operation;
-    
-    printf ("\t\tThe calculator\n\n");
-    
+
+    printf("\t\tThe calculator\n\n");
+
     printf("Insert two numbers\n");
-    scanf("%lf %lf", &firstNum, &secondNum);
-    
+    scanf_s("%lf %lf", &firstNum, &secondNum);
+    getchar();
+
     printf("Choose an operation: \n +\n -\n /\n *\n");
-    scanf("   %c", &operation);
-    
+    scanf_s("%c", &operation, 1);
+
     endRes = 0;
-    
+
     if (operation == '+') {
-        
+
         endRes = firstNum + secondNum;
-        printf ("The result: %.1lf\n", endRes);
+        printf("The result: %.1lf\n", endRes);
     }
-    else if(operation == '-') {
-        
+    else if (operation == '-') {
+
         endRes = firstNum - secondNum;
-        printf ("The result: %.1lf\n", endRes);
+        printf("The result: %.1lf\n", endRes);
     }
-    else if(operation == '*') {
-        
+    else if (operation == '*') {
+
         endRes = firstNum * secondNum;
-        printf ("The result: %.1lf\n", endRes);
+        printf("The result: %.1lf\n", endRes);
     }
-    else if(operation == '/') {
-        
+    else if (operation == '/') {
+
         endRes = firstNum / secondNum;
-        printf ("The result: %.1lf\n", endRes);
+        printf("The result: %.1lf\n", endRes);
     }
     else printf("Choose an operation out of list\n");
-    
+
+    getchar();
+    getchar();
+
     return 0;
-    
+
 }
