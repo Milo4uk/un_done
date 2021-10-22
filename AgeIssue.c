0c820689d04b23137be0c6f09e2fcb7c0418ab09
Milo4uk <92388527+Milo4uk@users.noreply.github.com>
2021-10-22 13:43:07 +0300

    Update AgeIssue.c

--- AgeIssue.c
+++ AgeIssue.c
@@ -6,30 +6,34 @@
 //
 
 #include <stdio.h>
+#include <locale.h>
 
 
 int main()
 {
     int Age, DayOfBirth, MonthOfBirth, YearOfBirth, RelevantDay, RelevantMonth, RelevantYear;
-      
-      printf ("Insert date of birth (day month year)\n");
-      scanf("%d %d %d", &DayOfBirth, &MonthOfBirth, &YearOfBirth);
-      
-      printf ("Insert date (day month year)\n");
-      scanf("%d %d %d", &RelevantDay, &RelevantMonth, &RelevantYear);
-      
+
+    setlocale(0, "Rus");
+
+    printf("Введите день рождения (день месяц год)\n");
+    scanf_s("%d %d %d", &DayOfBirth, &MonthOfBirth, &YearOfBirth);
+
+    printf("Введите сегодняшнюю дату (день месяц год)\n");
+    scanf_s("%d %d %d", &RelevantDay, &RelevantMonth, &RelevantYear);
+
     Age = RelevantYear - YearOfBirth;
-    
-    if (RelevantMonth > MonthOfBirth) {
-        printf("Her age is: %d\n ", Age + 1); //how to?????
+
+    if (Age % 10 == 0 || Age % 10 == 5 || Age % 10 == 6 || Age % 10 == 7 || Age % 10 == 8 || Age % 10 == 9) {
+
+        printf("Ей %d лет\n", Age);
+   }
+    else if (Age % 10 == 1) {
+        printf("Ей %d год\n", Age);
     }
-    else if (RelevantDay > DayOfBirth && RelevantMonth == MonthOfBirth) {
-        printf("Her age is: %d\n ", Age + 1);
-    }
-     else printf("Her age is: %d\n ", Age);
-          
-      getchar();
-      getchar();
-    
+    else  printf("Ей %d года\n", Age);
+
+    getchar();
+    getchar();
+
     return 0;
 }
